5bc1e808651409945401ef543a2d045e22510dad
Ivan <ivan.rybintsev@gmail.com>
2022-07-19 16:52:54 +0600

    Fix factoring logo in admin panel

--- upload/catalog/model/extension/payment/factoring004.php
+++ upload/catalog/model/extension/payment/factoring004.php
@@ -31,7 +31,7 @@ class ModelExtensionPaymentFactoring004 extends Model
 
         return array(
             'code'       => 'factoring004',
-            'title'      => '<img width="120px" src="catalog/view/theme/default/image/factoring004.png" alt="logo"></br>'.$this->language->get('text_factoring004_condition'),
+            'title'      => '<img width="120px" src="/catalog/view/theme/default/image/factoring004.png" alt="logo"></br>'.$this->language->get('text_factoring004_condition'),
             'terms'      => '',
             'sort_order' => 0
         );
